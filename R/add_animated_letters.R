b9ca3c83e14781a1bb9c915892adab60b9101c80
NicolasJBM <nicolas.jb.mangin@gmail.com>
2023-08-18 11:46:26 +0200

    DOCS: correct input help.

--- R/add_animated_letters.R
+++ R/add_animated_letters.R
@@ -3,7 +3,7 @@
 #' @author Nicolas Mangin
 #' @description Apply css animation separately for each letter of the entered text.
 #' @param text Character Text to which the animation should be applied letter by letter.
-#' @param animation Character. The kind of animation: "semi-fade-out", "grow", "shrink", "zoom-in", "strike", "fade-out", "fade-up", "fade-down", "fade-right", "fade-left", "highlight-red", "highlight-green", "highlight-blue"
+#' @param animation Character. The kind of animation: "bouncing", "infire", or "neon".
 #' @param color Character. CSS specification of the font size.
 #' @param size Character. CSS specification of the font color.
 #' @return Write the quarto code for the animation.
@@ -11,7 +11,7 @@
 
 
 add_animated_letters <- function(
-  text = "You can write here a sentence.", animation = "neon", color = "#FFF;", size = "1em"
+  text = "You can write here a sentence.", animation = "neon", color = "#FFF", size = "1em"
 ){
   text <- base::unlist(base::strsplit(text, " "))
   words <- base::character(0)
